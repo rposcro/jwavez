ee2c2333d606917c0e4ae74279930b325cfecbc4
Robert Poskrobek <rposkrob@akamai.com>
2019-01-18 17:17:57 +0100

    Expanded net tool with node configuration commands

--- jwavez-core/src/main/java/com/rposcro/jwavez/core/model/enums/BitLength.java
+++ jwavez-core/src/main/java/com/rposcro/jwavez/core/model/enums/BitLength.java
@@ -1,4 +1,34 @@
 package com.rposcro.jwavez.core.model.enums;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum BitLength {
+
+  BIT_LENGTH_8(8),
+  BIT_LENGTH_16(16),
+  BIT_LENGTH_32(32),
+  ;
+
+  @Getter
+  private int bitsNumber;
+
+  public int getBytesNumber() {
+    return bitsNumber / 8;
+  }
+
+  public static BitLength ofBytesNumber(int bytesNumber) {
+    switch(bytesNumber) {
+      case 1:
+        return BIT_LENGTH_8;
+      case 2:
+        return BIT_LENGTH_16;
+      case 4:
+        return BIT_LENGTH_32;
+      default:
+        throw new IllegalArgumentException("Unsupported number of bytes: " + bytesNumber);
+    }
+  }
 }
